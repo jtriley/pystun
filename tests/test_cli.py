4d38b5e391e222c6da371bb100f0bd84c33e3435
Nikolaos Korasidis <renelvon@gmail.com>
2014-11-05 05:58:57 +0200

    Verify default arguments in parser.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,9 +6,14 @@ from stun import cli
 class TestCLI(unittest.TestCase):
     """Test the CLI API."""
 
-    def test_cli_parser(self):
-        cli.make_argument_parser()
-        # TODO: Verify default arguments
+    def test_cli_parser_default(self):
+        parser = cli.make_argument_parser()
+        options = parser.parse_args([])
+
+        self.assertEqual(options.source_ip, cli.DEFAULTS['source_ip'])
+        self.assertEqual(options.source_port, cli.DEFAULTS['source_port'])
+        self.assertEqual(options.stun_port, cli.DEFAULTS['stun_port'])
+
         # TODO: Verify user arguments
 
 
